aaf9aff6fd1232f67d2552227d6232241d02cbea
pannuy <lovemanga171@gmail.com>
2015-07-14 23:44:41 +0700

    v.01

---
+++ fontrecents/version.php
@@ -0,0 +1,6 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2015051100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2015050500;        // Requires this Moodle version
+$plugin->component = 'block_fontrecents';
